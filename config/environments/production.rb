a2ea6071cd8d8c07896d2159e8f698f80a7c9013
jillian hoenig <jho3292@gmail.com>
2015-12-19 22:54:02 -0500

    Change application.rb config.assets.init to true.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -30,7 +34,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
