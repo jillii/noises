40fc319e2fec73716899e69f9969cbf12e0cf9dd
jillian hoenig <jho3292@gmail.com>
2015-12-19 23:14:59 -0500

    Fix spacing in user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 	def index
 	  if params[:search]    # if comments are requested by subject
-      @users = User.search(params[:search]).order('name ASC')
-    else # regular order
-      @users = User.all.order('name ASC')
-    end
+        @users = User.search(params[:search]).order('name ASC')
+      else # regular order
+        @users = User.all.order('name ASC')
+      end
 	end
 end
