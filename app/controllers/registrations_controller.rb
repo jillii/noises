e2e5dee866689c1fba3e9e3119c27925b0d0c27c
jillian hoenig <jho3292@gmail.com>
2015-12-22 21:38:34 -0500

    Undo override on destroy user method.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-  def destroy
-  	User.find(current_user).mixes.each do |mix|
-  		mix.destroy
-	end
-  	User.find(current_user).tracks.each do |track|
-  		track.destroy
-  	end
-  	super
-  end  
 
   private
 
