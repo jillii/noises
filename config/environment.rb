ab18b8edea3103e5870dbe8a9a48fd9649a618c2
jillii <jhoenig@isovera.com>
2015-12-25 08:59:07 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-# S3
-# S3_BUCKET='fotoes'
-# AWS_ACCESS_KEY_ID='AKIAILJLTE3NFGSHGOSA'
-# AWS_SECRET_ACCESS_KEY='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'
-
