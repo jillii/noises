7b15971db2fe0f5a249e7eb77686d5d6827c9b88
jillian hoenig <jho3292@gmail.com>
2016-01-09 17:50:54 -0500

    Precompile. Change color of links and link hover.

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-# S3
-# S3_BUCKET='add-your-bucket'
-# AWS_ACCESS_KEY_ID='your-access-key'
-# AWS_SECRET_ACCESS_KEY='your-secret-key'
-
