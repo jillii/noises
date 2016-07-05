f8238ed1328150860d95a15c142df500bed39058
jillian hoenig <jho3292@gmail.com>
2016-07-05 19:57:08 -0400

    And change port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "http://noises.herokuapp.com" }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 465,
     domain:               'gmail.com',
     user_name:            ENV['gmail_username'],
     password:             ENV['gmail_password'],
