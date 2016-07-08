78c381221a4b991e6f0d85c2db21830ce12d3ab1
jillian hoenig <jho3292@gmail.com>
2016-07-08 11:43:01 -0400

    Fox mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,14 +85,13 @@ Rails.application.configure do
   config.active_support.deprecation = :log
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries  = true
-  config.action_mailer.default_url_options = { :host => "http://noises.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => 'noises.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
     user_name:            ENV['gmail_username'],
     password:             ENV['gmail_password'],
-    authentication:       :plain,
-    enable_starttls_auto: true
+    authentication:       :plain
   }
 end
